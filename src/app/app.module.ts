16507a21e8b8db4187d516f28b1e42ac3a4d0294
alencarjulyana <89394255+alencarjulyana@users.noreply.github.com>
2022-09-24 14:51:53 -0300

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatCardModule} from '@angular/material/card';
 
+
 @NgModule({
   declarations: [
     AppComponent
